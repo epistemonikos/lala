574c34db26b59782354309c1a40fe86d4c639036
Antonio Vera <antonio.ignacio.vera@gmail.com>
2014-04-04 12:23:18 -0400

    Commented unused lines.

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,11 +22,14 @@ set :repo_url, 'git@github.com:epistemonikos/lala.git'
 # Default value for :pty is false
 set :pty, true
 
+require 'capistrano3-unicorn'
+
 # Default value for :linked_files is []
 # set :linked_files, %w{config/database.yml}
 
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{bin log tmp/pids}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/home/ubuntu/.rbenv/shims:/home/ubuntu/.rbenv/bin:$PATH" }
@@ -37,17 +40,17 @@ set :keep_releases, 3
 #set :rbenv_type, :user
 set :rbenv_ruby, '2.1.0'
 
-namespace :deploy do
+#namespace :deploy do
 
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      execute :touch, release_path.join('tmp/restart.txt')
-    end
-  end
+#  desc 'Restart application'
+#  task :restart do
+#    on roles(:app), in: :sequence, wait: 5 do
+#      # Your restart mechanism here, for example:
+#      execute :touch, release_path.join('tmp/restart.txt')
+#    end
+#  end
 
-  after :publishing, :restart
+#  after :publishing, :restart
 
 #  after :restart, :clear_cache do
 #    on roles(:web), in: :groups, limit: 3, wait: 10 do
@@ -58,7 +61,7 @@ namespace :deploy do
 #    end
 #  end
 
-end
+#end
 
 namespace :new_server do
   desc "Create deploy user"
@@ -217,10 +220,6 @@ end
 # Unicorn
 #------------------------------------------------------------------------------
 # # Load unicorn tasks
-
-set :linked_dirs, %w{bin log tmp/pids}
-
-require 'capistrano3-unicorn'
 
 #namespace :unicorn do
    #after "deploy:setup", "unicorn:setup"
